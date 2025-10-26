8a687a71d78007eeed9ad62cbbc1725729d06687
dt89 <tamalsarkartwoooo@gmail.com>
2025-10-26 16:09:51 +0000

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    console.log("usernames will be logged here - wip")
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
